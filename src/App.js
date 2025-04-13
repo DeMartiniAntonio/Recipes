ae6ee100f605bda37d6d1bc10f12644e73f5d58a
Antonio <antonio.sluzbeni@gmail.com>
2025-04-13 15:09:26 +0200

    Design

--- src/App.js
+++ src/App.js
@@ -2,14 +2,38 @@ import './App.css';
 import Navigation from './components/navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/imageLinkForm/ImageLinkForm';
+import Rank from './components/rank/Rank';
+import Particles from 'react-tsparticles';
+import { loadFull } from 'tsparticles';
+
+
+const particlesOptions ={
+  particles: {
+      number: {
+        value: 30,
+        density:{
+          enable: true,
+          value_area:800
+        }
+      },
+  }, 
+}
 
 function App() {
+  const particlesInit = async (engine) => {
+    await loadFull(engine);
+  };
+
   return (
     <div className="App">
+    <Particles className="particles" options={particlesOptions} init={particlesInit} />
       <Navigation />
       <Logo/>
+      <Rank />
       <ImageLinkForm />
-      {/*<FaceRecognition />*/}
+      {/*
+      cd /c/Users/Antonio/second-web/ReactFaceDetection
+      */}
     </div>
   );
 }
